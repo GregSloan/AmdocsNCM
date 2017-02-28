814f0e301d5a7e40a50158ddfdd6614b15d0b430
gsloan-qs <gregory.s@quali.com>
2017-02-28 14:56:00 -0800

    Update NCMpull.py

--- scripts/Python/NCMpull.py
+++ scripts/Python/NCMpull.py
@@ -1,7 +1,10 @@
-
 import paramiko
 from cloudshell.helpers.scripts import cloudshell_scripts_helpers as helper
 import re
+
+RES_ID = None
+RES_NAME = None
+API = None
 
 def _ssh_write(ssh, channel, command):
 
@@ -36,43 +39,38 @@ def _ssh_read(ssh, channel, prompt_regex):
 
 
 def _ssh_command(ssh, channel, command, prompt_regex):
-
+    global RES_ID
+    global RES_NAME
+    global API
+    if API:
+        API.WriteMessageToReservationOutput(RES_ID,
+                                            RES_NAME + ' sending command "<font color=teal>' + command + '</font>"')
     _ssh_write( ssh, channel, command + '\n')
     rv = _ssh_read( ssh, channel, prompt_regex)
     if '\n%' in rv.replace('\r', '\n'):
         es = 'CLI error message: ' + rv
 
         raise Exception(es)
+    if API:
+        API.WriteMessageToReservationOutput(RES_ID,
+                                            '-- command "<font color=lime> complete</font>')
     return rv
 
 resource_context = helper.get_resource_context_details()
 reservation_context = helper.get_reservation_context_details()
-res_id = reservation_context.id
-api = helper.get_api_session()
+RES_ID = reservation_context.id
+RES_NAME = resource_context.name
+API = helper.get_api_session()
 session = paramiko.SSHClient()
 session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 session.connect(resource_context.address, 22, 'root','amdocs')
 
 channel = session.invoke_shell()
 prompt = '.*]#'
-api.WriteMessageToReservationOutput(res_id, resource_context.name + ' sending command "mkdir -p /stage"')
 _ssh_command(session, channel, 'mkdir -p /stage', prompt)
-api.WriteMessageToReservationOutput(res_id, resource_context.name + '  --previous command complete')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + ' sending command "scp root@10.53.212.103:/stage/BSR9.9/bms/* /stage/"')
 _ssh_command(session, channel, 'scp root@10.53.212.103:/stage/BSR9.9/bms/* /stage/', '.*yes/no.*')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + '  --previous command complete')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + ' sending "yes"')
 _ssh_command(session, channel, 'yes','.*password.*')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + '  --previous command complete')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + ' sending password "*****"')
 _ssh_command(session, channel, 'amdocs','.*]#')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + '  --previous command complete')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + ' sending command "scp root@10.53.212.105:/stage/iso/* /stage/"')
 _ssh_command(session, channel, 'scp root@10.53.212.105:/stage/iso/* /stage/', '.*yes/no.*')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + '  --previous command complete')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + ' sending "yes"')
 _ssh_command(session, channel, 'yes','.*password.*')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + '  --previous command complete')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + ' sending password "*****"')
 _ssh_command(session, channel, 'amdocs', '.*]#')
-api.WriteMessageToReservationOutput(res_id, resource_context.name + '  --previous command complete')
